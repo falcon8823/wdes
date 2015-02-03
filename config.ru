a58046fd966a357cfefe36912687e9512a6775b4
Hayato OKUMOTO <falcon@falconsrv.net>
2015-02-03 23:59:57 +0900

    consoleコマンドを実装

--- config.ru
+++ config.ru
@@ -1,3 +1,4 @@
 require './init'
+require 'wdes/app'
 
 run WDES::App
--- init.rb
+++ init.rb
@@ -3,4 +3,4 @@ $:.unshift File.dirname(__FILE__)
 # libをパスに追加
 $:.unshift File.join(File.dirname(__FILE__), 'lib')
 
-require 'wdes/app'
+require 'wdes'
