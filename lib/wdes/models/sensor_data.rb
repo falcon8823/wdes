a8806240de1f5dab15ef3da8eda68c074d1ae5c9
Hayato OKUMOTO <falcon@falconsrv.net>
2015-02-04 01:39:47 +0900

    水分・照度のコメントメッセージを実装

--- lib/wdes/models/sensor_data.rb
+++ lib/wdes/models/sensor_data.rb
@@ -11,6 +11,49 @@ class SensorData
 
   before :create, :set_measured_at
 
+  # 照度
+  def illum
+    255 - illuminance
+  end
+
+  # 水分の状態
+  def moist_condition
+    if moisture > 170
+      "おぼれちゃうよ"
+    elsif moisture > 160
+      "みずおいしい"
+    elsif moisture > 140
+      "さいこう"
+    elsif moisture > 100
+      "いいかんじ"
+    elsif moisture > 60
+      "いいね"
+    elsif moisture > 50
+      "かわいてきた"
+    elsif moisture > 40
+      "みずちょうだい"
+    else
+      "かれちゃう…"
+    end
+  end
+
+  # 照度
+  def illum_condition
+    if illum > 170
+      "さいこうだ"
+    elsif illum > 140
+      "いいてんきだ"
+    elsif illum > 60
+      "あかるい"
+    elsif illum > 40
+      "ふぁー"
+    elsif illum > 20
+      "うすぐらい"
+    else
+      "まっくら"
+    end
+  end
+
   protected
 
   def set_measured_at
